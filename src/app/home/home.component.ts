b4ba8035ec165425812b80ae608f8d8653e9db70
Aleksandar Lukić <lukicaleksandar1992@gmail.com>
2023-12-09 17:52:07 +0100

    added imgs and img style

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FoodService } from '../services/food/food.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  foods:String[] = [];
+
+  constructor(private foodservice:FoodService) { }
 
   ngOnInit(): void {
+    this.foods = this.foodservice.getAll()
   }
 
 }
