90e488c1f3037a13360b6c930385165afaab5f10
Aleksandar Lukić <lukicaleksandar1992@gmail.com>
2024-01-01 23:22:20 +0100

    front folder fix

---
+++ front/src/app/services/cart/cart.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Cart } from 'src/app/shared/models/Cart';
+import { CartItem } from 'src/app/shared/models/CartItem';
+import { Food } from 'src/app/shared/models/food';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  private cart:Cart = new Cart();
+  private cartSubject: BehaviorSubject<Cart> = new BehaviorSubject(this.cart);
+  constructor() { }
+
+  addToCart(food: Food):void{
+    let cartItem = this.cart.items.find(item => item.food.id === food.id);
+    if(cartItem){
+      this.changeQuantity(food.id, cartItem.quantity + 1);
+      return;
+    }
+    this.cart.items.push(new CartItem(food));
+  }
+
+  removeFromCart(foodId:number): void{
+    this.cart.items = 
+    this.cart.items.filter(item => item.food.id != foodId);
+  }
+
+  changeQuantity(foodId:number, quantity:number){
+    let cartItem = this.cart.items.find(item => item.food.id === foodId);
+    if(!cartItem) return;
+    cartItem.quantity = quantity;
+  }
+
+  getCart():Cart{
+    return this.cart;
+  }
+
+  clearCart(){
+    this.cart = new Cart();
+  }
+
+  getCartObsevarble():Observable<Cart> {
+    return this.cartSubject.asObservable();
+  }
+  
+}
